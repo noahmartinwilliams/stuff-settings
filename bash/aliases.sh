52bce7d31e860087817a897a6e0ed9e8e84d31df
skynet1 <thecrazymancan@gmail.com>
2016-07-06 14:56:17 -0500

    Added a data2hex alias.

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -56,3 +56,4 @@ alias mkae='make'
 alias ding='cvlc ~/Sound/ding.wav'
 alias common_words="sed 's/[[:space:],.;:?\"!]\+/\n/g' | tr 'A-Z' 'a-z' | sed '/^[[:space:]]*$/d' | sort | uniq -c | sort -h -r"
 alias mkdir='mkdir -p'
+alias data2hex='od -A n -v -t x1 | tr -d " "'
