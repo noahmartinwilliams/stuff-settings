740d09bd8e513dad1fc0fb3d3c3ca20696400a14
skynet1 <thecrazymancan@gmail.com>
2016-07-09 14:50:00 -0500

    Added Gi, Mi, and Ki for Gibibytes, Mibibytes, and Kibibytes as constants.

--- bash/variables.sh
+++ bash/variables.sh
@@ -10,5 +10,8 @@ TMP=~/tmp
 LIBRARY_PATH=/usr/include:./include
 SPEECH="espeak"
 PATH="/bin:/usr/bin:/usr/local/bin:/usr/games:/usr/local/games:/sbin:/usr/sbin:/usr/local/sbin:~/bin"
+Ki=1024
+Mi=$(( 1024 * 1024))
+Gi=$(( 1024 * 1024 * 1024))
 
 source ~/bash/local-variables.sh
